e1f12458b5bc5eb6e7fe32a6ebad208fa03fa2bf
139Vaibhav <50567357+139Vaibhav@users.noreply.github.com>
2020-05-10 13:53:17 +0530

    Updated assign.php

--- final/assign.php
+++ final/assign.php
@@ -212,6 +212,14 @@ function closeForm3() {
      <!-- <i class="fa fa-caret-down"></i> -->
       </a>
 
+      <button class="dropdown-btn">Tasks 
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-container">
+        <a href="#">Project Deliverables</a>
+        <a href="#">Others</a>
+      </div>
+
       <a href="assigntasktable.php" style="color:white;">Task 
      <!-- <i class="fa fa-caret-down"></i> -->
       </a>
@@ -390,8 +398,9 @@ function closeForm3() {
     <div>
     <label style="font-size:17.5px; font-weight:100;">Assign Role: &nbsp </label>
     <select style="width: 20.9%; padding: 15px; margin: 5px 0 22px 0; border: none; background: #f1f1f1;" for="assignrole" id="assignrole" name="assignrole" style="margin-bottom:20px;">
-      <option value="lab incharge">Lab Incharge</option>
-      <option value="mentor">Mentor</option>
+      <option value="Collaborator">Collaborator</option>
+      <option value="Lab-Incharge">Lab-Incharge</option>
+      <option value="Mentor">Mentor</option>
     </select>
     </div><br>
 
@@ -481,19 +490,33 @@ function closeForm3() {
             $projectid = $_GET['rn'];
             $assignrole = $_POST['assignrole'];
             $assignname = $_POST['assignname'];
+            
+            // $result_check_repetition = $conn->prepare("select count(*) as num from `assign` where projectid = $projectid and username = $assignname");
+            // $result_check_repetition->execute();
+            // $row = $result_check_repetition->fetch(PDO::FETCH_ASSOC);
 
-          try {
-            if($projectid!=""){
-              $SQLInsert = "INSERT into assign(projectid, post ,username) VALUES ('$projectid','$assignrole','$assignname')";
-
-            $statement = $conn->prepare($SQLInsert);
-            $statement->execute();
-            echo $statement->rowCount() . " records UPDATED successfully";
-            }
+            $result_check_repetition = $conn->prepare("SELECT * FROM assign WHERE username = :assignname AND projectid = :projectid");
+            $result_check_repetition->bindParam(':assignname', $assignname);
+            $result_check_repetition->bindParam(':projectid', $projectid);
+            $result_check_repetition->execute();
+          if($result_check_repetition->rowCount()>0){
+              echo "<div style = \" text-align: center; position: absolute; top: 50%; left: 50%;\">$assignname is already assigned the task</div>";
           }
-          catch(PDOException $e)
-            {
-            echo $SQLInsert . "<br>" . $e->getMessage();
+          else{
+            try {
+              if($projectid!=""){
+                $SQLInsert = "INSERT into assign(projectid, post ,username) VALUES ('$projectid','$assignrole','$assignname')";
+  
+              $statement = $conn->prepare($SQLInsert);
+              $statement->execute();
+              echo $statement->rowCount() . "<div style = \" text-align: center; position: absolute; top: 50%; left: 50%;\"> records UPDATED successfully</div>";
+              }
             }
+            catch(PDOException $e)
+              {
+              echo $SQLInsert . "<br>" . $e->getMessage();
+              }
+          }
+
         }
         ?>
