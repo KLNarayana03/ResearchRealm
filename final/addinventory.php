f7ec504262b7e81d42ce461112ff61065c6e1e60
prinsusinghal <60322217+prinsusinghal@users.noreply.github.com>
2020-05-11 19:25:26 +0530

    Add files via upload

--- final/addinventory.php
+++ final/addinventory.php
@@ -214,12 +214,9 @@ function closeForm3() {
      <!-- <i class="fa fa-caret-down"></i> -->
       </a>
 
-      <button class="dropdown-btn">Inventory 
-        <!--<i class="fa fa-caret-down"></i> -->
-      </button>
-      <div class="dropdown-container">
-        
-      </div>
+      <a href="assignproject.php" style="color:white;">Inventory 
+     <!-- <i class="fa fa-caret-down"></i> -->
+      </a>
 
   </div>
 
@@ -228,14 +225,9 @@ function closeForm3() {
     <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-container">
-    <button class="dropdown-btn">Calendar Entries 
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container">
-        <a href="#">Meetings</a>
-        <a href="#">Reminders</a>
-        <a href="#">Deadlines</a>
-      </div>
+  <a href="assignproject.php" style="color:white;">Meeting
+     <!-- <i class="fa fa-caret-down"></i> -->
+      </a>
   </div>
 
   
@@ -295,13 +287,9 @@ function closeForm3() {
      <!-- <i class="fa fa-caret-down"></i> -->
       </a>
 
-      <button class="dropdown-btn">Tasks 
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container">
-        <a href="#">Project Deliverables</a>
-        <a href="#">Others</a>
-      </div>
+      <a href="assignproject.php" style="color:white;">Assigned Tasks 
+     <!-- <i class="fa fa-caret-down"></i> -->
+      </a>
   </div>
 
   
@@ -485,7 +473,7 @@ function closeForm3() {
 
             $statement = $conn->prepare($SQLInsert);
             $statement->execute();
-            echo $statement->rowCount() . "<div style = \" text-align: center; position: absolute; top: 50%; left: 50%;\"> Inventory added successfully </div>";
+            echo '<script>alert("Inventory added Successfully. Press Back to see it in table")</script>';
             }
           }
           catch(PDOException $e)
